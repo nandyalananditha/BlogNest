157f683e3a68bae15978ad3fa2b9fdbd3c881560
nandyalananditha <nandyalananditha@karunya.edu.in>
2026-05-30 21:40:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
-import CreatePost from "./components/CreatePost";
-import PostList from "./components/PostList";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>BlogNest</h1>
-
-      <CreatePost />
-
-      <hr />
-
-      <PostList />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
